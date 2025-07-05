4733e0fcb9b2f95a48ac6488c48f8db26a8b80e8
LikhithaShankar <likhithashankar823@gmail.com>
2025-07-05 15:31:31 +0530

    Update Transactiondetails.java

--- Bankingprojectt/src/main/java/com/banking/Transactiondetails.java
+++ Bankingprojectt/src/main/java/com/banking/Transactiondetails.java
@@ -20,7 +20,7 @@ public class Transactiondetails extends HttpServlet {
 
     String url = "jdbc:mysql://localhost:3306/project";
     String username = "root";
-    String password = "Likhitha@823";
+    String password = "password";
     Connection con = null;
     PreparedStatement pstmtDeposit = null;
     PreparedStatement pstmtWithdraw = null;
